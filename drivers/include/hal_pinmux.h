cd48243f7073dc6f420ac9eadf2a1f44597472f1
Hazel Zhang <hazel.zhang@unisoc.com>
2019-03-19 20:31:51 +0800

    modify pinmux code
    use "pin_reg" instead of "pin" to satisfy marlin3L's
    pin_reg difference from marlin3
    add marlin3L group pin_reg into pinmux code
    
    Signed-off-by: Hazel Zhang <hazel.zhang@unisoc.com>

--- drivers/include/hal_pinmux.h
+++ drivers/include/hal_pinmux.h
@@ -16,68 +16,6 @@
 
 /* pretend that array will cover pin functions */
 
-#define PINMUX_PIN_REG(x) \
-			(PIN_PIN_CRTL_REG0 + 0x4 * x)
-
-enum PINMUX_REG_UWP {
-	PIN_CRTL_REG0,
-	PIN_CRTL_REG1,
-	PIN_CRTL_REG2,
-	PIN_CRTL_REG3,
-	P_GPIO0,
-	P_GPIO1,
-	P_GPIO2,
-	P_GPIO3,
-	ESMD3,
-	ESMD2,
-	ESMD1,
-	ESMD0,
-	ESMCSN,
-	ESMSMP,
-	ESMCLK,
-	IISDO,
-	IISCLK,
-	IISLRCK,
-	IISDI,
-	MTMS,
-	MTCK,
-	XTLEN,
-	INT,
-	PTEST,
-	CHIP_EN,
-	RST_N,
-	WCI_2_RXD,
-	WCI_2_TXD,
-	U0TXD,
-	U0RXD,
-	U0RTS,
-	U0CTS,
-	U3TXD,
-	U3RXD,
-	RFCTL0,
-	RFCTL1,
-	RFCTL2,
-	RFCTL3,
-	RFCTL4,
-	RFCTL5,
-	RFCTL6,
-	RFCTL7,
-	SD_D3,
-	SD_D2,
-	SD_D1,
-	SD_D0,
-	SD_CLK,
-	SD_CMD,
-	GNSS_LNA_EN,
-	U1TXD,
-	U1RXD,
-	U1RTS,
-	U1CTS,
-	PCIE_CLKREQ_L,
-	PCIE_RST_L,
-	PCIE_WAKE_L,
-};
-
 static inline void __pin_enbable(u8_t enable)
 {
 	u32_t regval;
@@ -90,48 +28,80 @@ static inline void __pin_enbable(u8_t enable)
 	}
 }
 
-static inline void uwp_pmux_func_clear(u32_t pin)
+static inline void uwp_pmux_func_clear(u32_t pin_reg)
 {
-	u32_t pin_reg = PINMUX_PIN_REG(pin);
-	u32_t conf = sys_read32(pin_reg);
+	#if defined(CONFIG_SOC_UWP5661)
+		u32_t conf = sys_read32(pin_reg);
 
-	conf &= (~(PMUX_PIN_FUNC(1)));
-	sys_write32(conf, pin_reg);
+		conf &= (~PIN_FUNC_MSK);
+		sys_write32(conf, pin_reg);
+	#elif defined(CONFIG_SOC_UWP5662)
+		sci_glb_clr(pin_reg, PIN_FUNC_MSK);
+	#endif
 }
 
-static inline void uwp_pmux_func_set(u32_t pin, u32_t func)
+static inline void uwp_pmux_func_set(u32_t pin_reg, u32_t func)
 {
-	u32_t pin_reg = PINMUX_PIN_REG(pin);
-	u32_t conf = sys_read32(pin_reg);
+	#if defined(CONFIG_SOC_UWP5661)
+		u32_t conf = sys_read32(pin_reg);
 
-	conf |= (PMUX_PIN_FUNC(func));
-	sys_write32(conf, pin_reg);
+		conf |= func;
+		sys_write32(conf, pin_reg);
+	#elif defined(CONFIG_SOC_UWP5662)
+		if (func & PIN_FUNC_MSK) {
+			u32_t conf = sys_read32(pin_reg);
+
+			conf |= func;
+			sci_glb_set(pin_reg, conf);
+		}
+		if (func & (~PIN_FUNC_MSK)) {
+			u32_t conf = sys_read32(GROUP(pin_reg));
+
+			conf |= (func & (~PIN_FUNC_MSK));
+			sci_glb_set(GROUP(pin_reg), conf);
+		}
+	#endif
 }
 
-static inline void uwp_pmux_get(u32_t pin, u32_t *func)
+static inline void uwp_pmux_get(u32_t pin_reg, u32_t *func)
 {
-	u32_t pin_reg = PINMUX_PIN_REG(pin);
 	u32_t conf = sys_read32(pin_reg);
 
 	*func = conf;
 }
 
-static inline void uwp_pmux_pin_pullup(u32_t pin)
+static inline void uwp_pmux_pin_pullup(u32_t pin_reg)
 {
-	u32_t pin_reg = PINMUX_PIN_REG(pin);
-	u32_t conf = sys_read32(pin_reg);
+	#if defined(CONFIG_SOC_UWP5661)
+		u32_t conf = sys_read32(pin_reg);
 
-	conf |= PIN_FPU_EN;
-	sys_write32(conf, pin_reg);
+		conf &= (~PIN_FPU_MSK);
+		conf |= PIN_FPU_EN;
+		sys_write32(conf, pin_reg);
+	#elif defined(CONFIG_SOC_UWP5662)
+		u32_t conf = sys_read32(GROUP(pin_reg));
+
+		sci_glb_clr(GROUP(pin_reg), PIN_FPU_MSK);
+		conf |= PIN_FPU_EN;
+		sci_glb_set(GROUP(pin_reg), conf);
+	#endif
 }
 
-static inline void uwp_pmux_pin_pulldown(u32_t pin)
+static inline void uwp_pmux_pin_pulldown(u32_t pin_reg)
 {
-	u32_t pin_reg = PINMUX_PIN_REG(pin);
-	u32_t conf = sys_read32(pin_reg);
+	#if defined(CONFIG_SOC_UWP5661)
+		u32_t conf = sys_read32(pin_reg);
 
-	conf |= PIN_FPD_EN;
-	sys_write32(conf, pin_reg);
+		conf &= (~PIN_FPU_MSK);
+		conf |= PIN_FPD_EN;
+		sys_write32(conf, pin_reg);
+	#elif defined(CONFIG_SOC_UWP5662)
+		u32_t conf = sys_read32(GROUP(pin_reg));
+
+		sci_glb_clr(GROUP(pin_reg), PIN_FPU_MSK);
+		conf |= PIN_FPD_EN;
+		sci_glb_set(GROUP(pin_reg), conf);
+	#endif
 }
 
 #endif	/* _STM32_PINMUX_H_ */
